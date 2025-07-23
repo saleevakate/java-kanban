fc3b5c6a87acb900346e356232e68b7e5b843034
saleevakate <saleevakate@yandex.ru>
2025-07-23 23:08:56 +0500

    ТЗ 7 готово

--- test/manager/FileBackedTaskMenagerTest.java
+++ test/manager/FileBackedTaskMenagerTest.java
@@ -1,7 +1,5 @@
 package manager;
 
-import manager.FileBackedTaskManager;
-import manager.TaskManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -11,37 +9,39 @@ import tasks.Task;
 import tasks.TaskStatus;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 class FileBackedTaskManagerTest {
 
     @TempDir
     protected Path tempDir;
 
-    private FileBackedTaskManager manager;
+    private FileBackedTaskManager taskManager;
     private Path savedTasksFile;
+    private final HashMap<Integer, Task> tasks = new HashMap<>();
+    private final HashMap<Integer, Epic> epics = new HashMap<>();
+    private final HashMap<Integer, Subtask> subtasks = new HashMap<>();
 
     @BeforeEach
     void setUp() {
         savedTasksFile = tempDir.resolve("tasks.csv");
-        manager = new FileBackedTaskManager(savedTasksFile.toFile());
+        taskManager = new FileBackedTaskManager(savedTasksFile.toFile());
 
         Task task = new Task(1, "Таск", "Описание", TaskStatus.NEW);
         Epic epic = new Epic(2, "Эпик", "Описание", TaskStatus.NEW);
         Subtask subtask = new Subtask(3, "Сабтаск", "Описание", 2, TaskStatus.NEW);
 
-        manager.createTask(task);
-        manager.createEpic(epic);
-        manager.createSubtask(subtask, 2);
+        taskManager.createTask(task);
+        taskManager.createEpic(epic);
+        taskManager.createSubtask(subtask, 2);
     }
 
     @Test
-    void testSave() {
-        try {
+    void testSave() throws IOException {
             String content = Files.readString(savedTasksFile);
             String[] lines = content.split("\n");
             assertEquals("id,type,name,status,description,epic", lines[0].trim());
@@ -56,10 +56,33 @@ class FileBackedTaskManagerTest {
             assertTrue(content.contains(expectedEpicLine));
             assertTrue(content.contains(expectedSubtaskLine));
             assertTrue(lines[4].isBlank());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
+    @Test
+    void testLoadFromFile() throws IOException {
+        assertTrue(Files.exists(savedTasksFile));
+        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(savedTasksFile.toFile());
 
+        Task loadedTask = loadedManager.getTaskById(1);
+        assertNotNull(loadedTask, "Задача не загружена");
+        assertEquals(1, loadedTask.getId());
+        assertEquals("Таск", loadedTask.getName());
+        assertEquals(TaskStatus.NEW, loadedTask.getTaskStatus());
+        assertEquals("Описание", loadedTask.getDescription());
+
+        Epic loadedEpic = loadedManager.getEpicById(2);
+        assertNotNull(loadedEpic, "Эпик не загружен");
+        assertEquals(2, loadedEpic.getId());
+        assertEquals("Эпик", loadedEpic.getName());
+        assertEquals(TaskStatus.NEW, loadedEpic.getTaskStatus());
+        assertEquals("Описание", loadedEpic.getDescription());
+
+        Subtask loadedSubtask = loadedManager.getSubtaskById(3);
+        assertNotNull(loadedSubtask, "Подзадача не загружена");
+        assertEquals(3, loadedSubtask.getId());
+        assertEquals("Сабтаск", loadedSubtask.getName());
+        assertEquals(TaskStatus.NEW, loadedSubtask.getTaskStatus());
+        assertEquals("Описание", loadedSubtask.getDescription());
+        assertEquals(2, loadedSubtask.getEpicId());
+    }
 }
